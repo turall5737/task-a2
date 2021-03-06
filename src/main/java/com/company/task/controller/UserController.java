88c5da57314ba7ce736eab3d73ba92cbf873efdf
turala@crocusoft.com <Turallir>
2021-03-06 11:04:47 +0400

    first commit

---
+++ src/main/java/com/company/task/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.company.task.controller;
+
+import com.company.task.model.dto.UserDto;
+import com.company.task.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void add(@Valid @RequestBody UserDto userDto){
+        log.debug("/users , userDto = {}",userDto);
+       userService.add(userDto);
+    }
+}
